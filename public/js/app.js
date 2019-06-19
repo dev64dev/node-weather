94e7c32d4fab0908b609967ba6c21c18c1dc0419
dev64 <dev64dev@gmail.com>
2019-06-19 09:28:06 +0300

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,12 @@
+console.log("Client side JAVASCRIPT");
+
+fetch('http://localhost:3000?address=boston').then((response)=>{
+    response.json().then((data) => {
+        if (data.error) {
+            console.log(data.error);
+        } else {
+            console.log(data.location);
+            console.log(data.forecast);
+        }
+    });
+});
---
+++ src/app.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const express = require('express');
+const geocode = require('./utils/geocode.js')
+const forecast = require('./utils/forecast.js')
+const app = express();
+
+// console.log(__dirname);
+// console.log(path.join(__dirname, '../public'));
+const publicDirectoryPath = path.join(__dirname, '../public');
+
+app.set('view engine', 'hbs');
+app.use(express.static(publicDirectoryPath));
+
+app.get('/about', (req, res) => {
+    res.send({
+        name: 'Yaniv',
+        Age: '39'
+
+    });
+})
+app.get('/help', (req, res) => {
+    res.send('Help Page');
+})
+
+
+app.get('', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide an address!'
+        })
+    }
+
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send({ error })
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            }
+
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.');
+
+});
